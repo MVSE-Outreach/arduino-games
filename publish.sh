b0b4d8c1230abb7a3120307b6de31b9d5cb77d2c
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-02-26 17:40:47 +0000

    Checkout master after publishing

--- publish.sh
+++ publish.sh
@@ -1,1 +1,5 @@
-git push origin master && git checkout gh-pages && git merge master && git push origin gh-pages
+git push origin master
+git checkout gh-pages
+git merge master
+git push origin gh-pages
+git checkout master
