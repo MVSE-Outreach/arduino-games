2352683dd5eeba1801ab6e157c02f7217cb183e6
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-02-27 13:37:51 +0000

    Be consistant in how the screen object is referenced.

--- arduino-src/screen-test/screen-test.ino
+++ arduino-src/screen-test/screen-test.ino
@@ -7,7 +7,7 @@
 #define RST  1
 
 // create an instance of the library
-TFT TFTscreen = TFT(CS, DC, RST);
+TFT screen = TFT(CS, DC, RST);
 
 // Use this to count upwards forever.
 int counter = 0;
@@ -18,33 +18,33 @@ char letters[10];
 void setup() {
 
 // Put this line at the beginning of every sketch that uses the GLCD:
-TFTscreen.begin();
+screen.begin();
 
 // clear the screen with a white background
-TFTscreen.background(255,255,255);
+screen.background(255,255,255);
 // Set the fill colour to white.
-TFTscreen.fill(255,255,255);
+screen.fill(255,255,255);
 // set the font colour to black
-TFTscreen.stroke(0, 0, 0);
+screen.stroke(0, 0, 0);
 // set the font size
-TFTscreen.setTextSize(2);
+screen.setTextSize(2);
 // ste the font size very large for the loop
-TFTscreen.setTextSize(2);
+screen.setTextSize(2);
 }
 
 void loop() {
 // Write hello world to the screen.
-TFTscreen.text("Hello World!", 1, 20);
+screen.text("Hello World!", 1, 20);
 
 // The screen is dumb and always writes over itself.
 // Blank the part of the screen that will change.
 
-TFTscreen.rect(1,40,100,40);
+screen.rect(1,40,100,40);
 
 // Print out the counter number.
 String count = String(counter);
 count.toCharArray(letters,10);
-TFTscreen.text(letters, 1,40);
+screen.text(letters, 1,40);
 
 // wait for a moment then loop back round.
 delay(500);
